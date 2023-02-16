ecac5faf16986e1792ee418624bb28f84468a5fe
Yunus Emre <42357900+YuunsGit@users.noreply.github.com>
2023-02-16 17:18:54 +0300

    Minor design changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -351,17 +351,19 @@ export default function Home() {
       <footer
         className={`${grotesk.variable} bg-primary-300 py-6 text-center font-sans`}
       >
-        <a
-          href="https://github.com/YuunsGit/yuuns-tech"
-          rel="noreferrer"
-          target="_blank"
-          className="max-w-9/12 relative inline-block text-sm text-secondary duration-100 after:absolute after:-bottom-1 after:left-0 after:h-0.5 after:w-full after:origin-center after:scale-x-0 after:bg-secondary after:duration-150 after:content-[''] sm:text-base sm:hover:after:scale-x-100"
-        >
-          <code>
-            Illustrated, Designed & Built by{" "}
-            <span className="whitespace-nowrap">Yunus Emre Kepenek</span>
-          </code>
-        </a>
+        <div className="mx-auto w-9/12">
+          <a
+            href="https://github.com/YuunsGit/yuuns-tech"
+            rel="noreferrer"
+            target="_blank"
+            className="relative text-sm text-secondary duration-100 after:absolute after:-bottom-1 after:left-0 after:h-0.5 after:w-full after:origin-center after:scale-x-0 after:bg-secondary after:duration-150 after:content-[''] sm:text-base sm:hover:after:scale-x-100"
+          >
+            <code>
+              Illustrated, Designed & Built by{" "}
+              <span className="whitespace-nowrap">Yunus Emre Kepenek</span>
+            </code>
+          </a>
+        </div>
       </footer>
     </>
   );

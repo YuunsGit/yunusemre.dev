5ebbfd15b4859bf9f22c6de4d834a159f223bb9b
Yunus Emre <42357900+YuunsGit@users.noreply.github.com>
2023-03-12 19:46:15 +0300

    Updated transition image as undraggable

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -16,6 +16,7 @@ const Main = ({ font }: { font: string }) => {
         src={TransitionWave}
         alt="Wave picture for color transition"
         className="2xl:w-100 min-w-[300%] select-none md:min-w-[200%] lg:min-w-full"
+        draggable="false"
       />
       <div className="space-y-32 bg-primary-300 py-24 text-white md:space-y-56 md:py-40">
         <About />

b145a855d5c8ed38f1977bf2375be05342fd6046
YuunsGit <yunus.emre.kepenek@outlook.com>
2023-05-18 20:29:40 +0300

    SSR was a mistake

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -27,7 +27,7 @@ const Projects = () => {
             alt="Mascot of Sinbad DigiBox"
             className="absolute -left-44 -top-32 hidden h-4/5 -rotate-12 lg:block"
           />
-          <div className="relative z-10 flex flex-col-reverse justify-between gap-y-8 gap-x-0 rounded-3xl bg-sky-50 px-7 py-9 text-center text-ternary-500 duration-200 group-hover:bg-sky-100 sm:p-12 md:flex-row md:gap-x-20 md:gap-y-0 md:text-left">
+          <div className="relative z-10 flex flex-col-reverse justify-between gap-x-0 gap-y-8 rounded-3xl bg-sky-50 px-7 py-9 text-center text-ternary-500 duration-200 group-hover:bg-sky-100 sm:p-12 md:flex-row md:gap-x-20 md:gap-y-0 md:text-left">
             <article>
               <h4 className="text-2xl font-extrabold">Sinbad DigiBox</h4>
               <h5 className="text-xl font-semibold">Lead & Web Mentor</h5>
@@ -49,7 +49,7 @@ const Projects = () => {
               className="mx-auto h-auto w-10/12 md:w-1/3"
             />
           </div>
-          <ArrowRightCircleIcon className="absolute right-0 top-0 bottom-0 z-0 my-auto hidden h-14 text-sky-100 duration-200 group-hover:translate-x-16 md:block" />
+          <ArrowRightCircleIcon className="absolute bottom-0 right-0 top-0 z-0 my-auto hidden h-14 text-sky-100 duration-200 group-hover:translate-x-16 md:block" />
         </a>
         <a
           href="https://conversions.yuuns.tech/"
@@ -57,7 +57,7 @@ const Projects = () => {
           rel="noreferrer"
           className="group relative"
         >
-          <div className="relative z-10 flex flex-col-reverse justify-between gap-y-8 gap-x-0 rounded-3xl bg-stone-800 px-7 py-9 text-center duration-200 group-hover:bg-stone-900 sm:p-12 md:flex-row md:gap-x-20 md:gap-y-0 md:text-left">
+          <div className="relative z-10 flex flex-col-reverse justify-between gap-x-0 gap-y-8 rounded-3xl bg-stone-800 px-7 py-9 text-center duration-200 group-hover:bg-stone-900 sm:p-12 md:flex-row md:gap-x-20 md:gap-y-0 md:text-left">
             <article>
               <h4 className="text-2xl font-extrabold">Conversions</h4>
               <h5 className="text-xl font-semibold">Author</h5>
@@ -79,7 +79,7 @@ const Projects = () => {
               className="mx-auto h-auto w-10/12 md:w-1/3"
             />
           </div>
-          <ArrowRightCircleIcon className="absolute right-0 top-0 bottom-0 z-0 my-auto hidden h-14 text-stone-900 duration-200 group-hover:translate-x-16 md:block" />
+          <ArrowRightCircleIcon className="absolute bottom-0 right-0 top-0 z-0 my-auto hidden h-14 text-stone-900 duration-200 group-hover:translate-x-16 md:block" />
         </a>
         <a
           href="https://github.com/YuunsGit/legionary"
@@ -106,10 +106,10 @@ const Projects = () => {
             <Image
               src={HeMan}
               alt="Legionary logo"
-              className="my-auto mx-auto w-10/12 md:w-1/3"
+              className="mx-auto my-auto w-10/12 md:w-1/3"
             />
           </div>
-          <ArrowRightCircleIcon className="absolute right-0 top-0 bottom-0 z-0 my-auto hidden h-14 text-stone-200 duration-200 group-hover:translate-x-16 md:block" />
+          <ArrowRightCircleIcon className="absolute bottom-0 right-0 top-0 z-0 my-auto hidden h-14 text-stone-200 duration-200 group-hover:translate-x-16 md:block" />
         </a>
       </div>
     </div>

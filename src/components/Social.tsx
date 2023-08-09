89424e053c9ac3fd26f79577cc66af161b25eb45
YuunsGit <yunus.emre.kepenek@outlook.com>
2023-08-09 18:24:17 +0300

    Image optimization

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -13,6 +13,8 @@ const Socials: React.FC<Props> = ({ name, link, icon }) => {
       <Image
         src={icon}
         alt={`My ${name} account`}
+        height={24}
+        width={24}
         className="h-5 w-5 duration-150 sm:h-6 sm:w-6 sm:grayscale sm:hover:scale-125 sm:hover:grayscale-0"
         priority
       />
